892a114f149e6b6dad105aa0efb93650979fa813
NaviGracia <ivagartor2@alu.edu.gva.es>
2024-03-26 20:48:51 +0100

    a

--- Balastro/src/App.java
+++ Balastro/src/App.java
@@ -1,10 +1,53 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class App {
+    static Scanner sc = new Scanner(System.in);
+    
+    static public void consulatBD(Statement st, Connection conexion) throws Exception{
+         //Consulta
+         conexion.commit();
+         ResultSet rs = st.executeQuery("SELECT * FROM jokers");
+         int n = 0;
+         System.out.println("Jokers Balatro:");
+         while (rs.next()) {
+             System.out.println(rs.getString(1) + " " + rs.getString(2) + ": " + rs.getString(3));
+             n++;
+         }
+         rs.close();
+         st.close();
+         conexion.close();
+    }
+
+    static public void insertarDatos(Connection conexion){
+         //Insertar Datos
+         String sentenciaSQL = "INSERT INTO jokers VALUES(?, ?)";
+         PreparedStatement sentencia = null;
+ 
+         try{
+             sentencia = conexion.prepareStatement(sentenciaSQL);
+             sentencia.setInt(1, 2);
+             sentencia.setString(2, "Cuatro Dedos");
+             sentencia.setString(3, "Las escaleras y color pueden hacerse con cuatro cartas.");
+             sentencia.executeUpdate();
+         }catch (SQLException sqle) {
+             sqle.printStackTrace();
+         } finally {
+             if (sentencia != null)
+                 try {
+                     sentencia.close();
+                 } catch (SQLException sqle) {
+                     sqle.printStackTrace();
+                 }
+             } 
+    }
     public static void main(String[] args) throws Exception {
+        //Conectar BD
         try {
             Class.forName("org.postgresql.Driver");
         } catch (Exception e) {
@@ -17,17 +60,19 @@ public class App {
         Statement st = conexion.createStatement();
         conexion.setAutoCommit(false);
 
-        //Consulta
-        conexion.commit();
-        ResultSet rs = st.executeQuery("SELECT * FROM jokers");
-        int n = 0;
-        System.out.println("Jokers Balatro:");
-        while (rs.next()) {
-            System.out.println(rs.getString(1) + " " + rs.getString(2) + ": " + rs.getString(3));
-            n++;
+        System.out.println("Menú BD Balatro:" + "\n 1. Insertar Datos" + "\n 2. Consultar");
+        int eleccion = sc.nextInt();
+        switch (eleccion) {
+            case 1:
+                insertarDatos(conexion);
+                break;
+            case 2:
+                consulatBD(st, conexion);
+                break;
+            default:
+                break;
         }
-        rs.close();
-        st.close();
-        conexion.close();
+
+       
     }
 }
